a4b75331b7eff2c9d514a020ce57ed5d33cd0dd1
JoshCaneday <146874169+JoshCaneday@users.noreply.github.com>
2024-09-03 18:55:41 -0700

    Updated everything

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('nameForm');
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        
+        const name = document.getElementById('name').value;
+        localStorage.setItem('name', name);
+        
+        window.location.href = 'submission.html';
+    });
+});
