fbf0f8e2611d819d21458c1eca9efd41433aea78
JoshCaneday <146874169+JoshCaneday@users.noreply.github.com>
2024-09-13 19:33:50 -0700

    corrected sql export

--- ucsd_classes2.sql
+++ ucsd_classes2.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `ucsd_classes` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `ucsd_classes`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: localhost    Database: ucsd_classes
@@ -112,4 +114,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-13 18:39:03
+-- Dump completed on 2024-09-13 18:52:53
